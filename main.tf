e40a54f95feff19a2b502583c6543c3f645b3a8c
shegyam <shegyam.gcp@gmail.com>
2023-01-04 16:14:53 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,46 @@
-module "gke_cluster" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = "shegyam-001"
-  name                       = "gke-cluster-1"
-  region                     = "us-central1"
-  network                    = "default"
-  subnetwork                 = "default"
-  ip_range_pods              = "default"
-  ip_range_services          = "default"
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = true
-  network_policy             = false
+variable "project_id" {
+  description = "The project ID of your project"
+  default = "shegyam-001"
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+}
+
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file (C:\Users\GOMASA SHIVA KUMAR\Downloads\shegyam-001-976b813336a6.json)
-  project_id = "shegyam-001"
-  region = "us-central1"
+credentials = "trans-shuttle-373506-2479780ec576.json"
+project = "shegyam gcp1"
+region = "us-central1"
 }
